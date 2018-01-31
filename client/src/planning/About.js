c8dfb8e394a3aac67150be69843d414f74d3db6c
sweeneyyy <sweeneyarnett@gmail.com>
2018-01-30 17:46:23 -0800

    css ing

--- client/src/planning/About.js
+++ client/src/planning/About.js
@@ -5,20 +5,54 @@ class About extends Component {
   render() {
     return(
       <div className="About-container">
-        <h3>About Section Coming Soon!</h3>
-        <div className="grid-container">
-          <div className="profile">person 1</div>
-          <div className="profile">person 2</div>
-          <div className="profile">person 3</div>
+        <h5>team</h5>
+        <div className="container">
+        <div className="row">
+          <div className="col s12 m4">
+            <div className="card">
+              <div className="card-image">
+                <img src="https://placebear.com/500/600" />
+              </div>
+              <div className="card-content">
+                <p>Winston Summers</p>
+              </div>
+              <div className="card-action">
+                <a href="#">github</a>
+              </div>
+            </div>
+          </div>
+          <div className="col s12 m4">
+            <div className="card">
+              <div className="card-image">
+                <img src="https://placebear.com/g/500/600" />
+              </div>
+              <div className="card-content">
+                <p>Tony Phan</p>
+              </div>
+              <div className="card-action">
+                <a href="#">github</a>
+              </div>
+            </div>
+          </div>
+          <div className="col s12 m4">
+            <div className="card">
+              <div className="card-image">
+                <img src="https://placebear.com/500/600" />
+              </div>
+              <div className="card-content">
+                <p>Sweeney Arnett</p>
+              </div>
+              <div className="card-action">
+                <a href="#">github</a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+    </div>
     )
   }
 }
-
-
-
-
 
 
 export default About;
--- client/src/planning/HowItWorks.js
+++ client/src/planning/HowItWorks.js
@@ -4,11 +4,13 @@ class HowItWorks extends Component {
   render() {
     return(
       <div className="About-container">
-        <h3>How It Works Section Coming Soon!</h3>
-        <div className="grid-container">
-          <div className="how-it-works">info1</div>
-          <div className="how-it-works">info2</div>
-          <div className="how-it-works">info3</div>
+        <h5>how it works</h5>
+        <div className="container">
+          <div className="row">
+            <div className="how-it-works col s12 m4">explore</div>
+            <div className="how-it-works col s12 m4">plan</div>
+            <div className="how-it-works col s12 m4">organize</div>
+          </div>
         </div>
       </div>
     )

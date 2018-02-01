eeaeb7643ed5daae5368f991caa272ca6fc5b012
sweeneyyy <sweeneyarnett@gmail.com>
2018-01-31 18:27:00 -0800

    getting api data from back to front

--- client/src/planning/Search.js
+++ client/src/planning/Search.js
@@ -5,31 +5,36 @@ class Search extends Component {
   constructor(props){
     super(props)
     this.state = {
-      query: ''
+      query: '',
+      name: []
     }
   }
 
 
   handleInputChange = (e) => {
-    this.setState({query:e.target.value});
+    this.setState({query: e.target.value});
   }
 
-  perventing = (e) => {
+  preventing = (e) => {
     e.preventDefault();
     axios.post('/saved/results', {
-      location: this.state.query
+      location: this.state.query,
     }).then((res) => {
-      console.log(res);
-
+      console.log("response is", res.data.businesses[0].name);
+      this.setState({name: res.data.businesses[0].name});
+      console.log("name...", this.state.name);
+      console.log("got to then", this.state.query)
+      this.setState({ results: e.prettyJson})
+      console.log(this.state.results);
     }).catch((err) => {
-      console.log("error:"+err);
+      console.log("error:", err);
     })
   }
 
   render() {
     return(
       <div className="Search container">
-      	<form className="form" onSubmit={this.perventing}>
+      	<form className="form" onSubmit={this.preventing}>
       		<input type="text"
                 name="location"
                 placeholder="Enter Location"
--- routes/saved.js
+++ routes/saved.js
@@ -9,56 +9,30 @@ var jwt = require('jsonwebtoken');
 const yelp = require('yelp-fusion');
 const apikey = process.env.API_KEY;
 const client = yelp.client(apikey);
-// const API_URL = 'https://api.yelp.com/v3/events';
 
-
-// function yelpSearch(searchTerm, location, callback){
-//   client.search({
-//     term: searchTerm,
-//     location: location
-//   }).then(response => {
-//     response.jsonBody.businesses.forEach(function(item){
-//     });
-//     callback(response.jsonBody.businesses);
-//   }).catch(e => {
-//     console.log(e);
-//   });
-// }
-
-//yelp post route
-// router.post('/results', function(req, res) {
-//   console.log(req.body);
-//   yelpSearch(req.body.location, 'Seattle', function(businesses){
-//     res.render('/results', {businesses: businesses});
-//   });
-// });
 
 router.post('/results', function(req, res, callback){
-  console.log(req.body);
+  console.log("req.body is", req.body);
   const searchRequest = {
-    location: "Seattle, wa" //search location from front end
+    location: req.body.location,  //search location from front end
+    limit: 10
   }
   client.search(searchRequest).then(response => {
     const result = response.jsonBody;
     const prettyJson = JSON.stringify(result, null, 4);
-    console.log(prettyJson);
-  }).catch(err => {
-    console.log(err);
+    console.log("this is prettyJson", prettyJson);
+    res.send(prettyJson);
+  }).catch((err) => {
+    console.log("error:", err);
   });
 });
-// router.get('/results', function(req, res, callback){
-//   const searchRequest = {
-//     location: req.body.location //search location from front end
-//   }
-//   client.search(searchRequest).then(response => {
-//     const result = response.jsonBody;
-//     const prettyJson = JSON.stringify(result, null, 4);
-//     console.log(prettyJson);
-//   }).catch(err => {
-//     console.log(err);
-//   });
-// });
+
 
 //POST - save restaurant or event to db
+
+
+
+
+
 
 module.exports = router;

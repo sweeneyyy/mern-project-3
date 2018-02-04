79527585ec6c71dbf0a7fd0108246bd6813daa5d
rallysport206 <phan.tony125@gmail.com>
2018-02-04 12:17:48 -0800

    profile saved in cards and list side by side

--- client/src/planning/RestaurantsSaved.js
+++ client/src/planning/RestaurantsSaved.js
@@ -3,23 +3,24 @@ import React, { Component } from 'react';
 class RestaurantsSaved extends Component{
   render(){
     return(
-          <div className='card small'>
-            <div className="card-image-business">
-              <img className="saved-img" src={this.props.business.imgurl} alt={this.props.business.name} />
+        <div className='row'>
+          <div className='card'>
+                <div className="card-image-saved">
+                  <img className="saved-img" src={this.props.business.imgurl} alt={this.props.business.name} />
+                </div>
+                <div className="card-content">
+                <ul>
+                  <li className="restaurants-saved">
+                    <h5>{this.props.business.name}</h5>
+                    <span><p>Category: <bold>{this.props.business.category} </bold>
+                    | Rating: {this.props.business.rating}</p></span>
+                  <a href={this.props.business.url}><img className="yelp" src="https://i.imgur.com/cSLthnw.png" alt="yelp"/></a>
+                    <input type="button" value="X" id={this.props.business.id} onClick={this.props.saveClick} />
+                  </li>
+                </ul>
+              </div>
             </div>
-            <div className="card-content">
-            <ul>
-              <li className="restaurants-saved">
-                <h5>{this.props.business.name}</h5>
-                <span><p>Category: <bold>{this.props.business.category} </bold>
-                | Rating: {this.props.business.rating}</p></span>
-                <a href={this.props.business.url}>Details on Yelp! </a>
-                <input type="button" value="X" id={this.props.business.id} onClick={this.props.saveClick} />
-                <hr />
-              </li>
-            </ul>
           </div>
-        </div>
     );
   }
 }

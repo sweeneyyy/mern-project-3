2337d8e896ecfe443b82d413462a8ca134bfddc6
sweeneyyy <sweeneyarnett@gmail.com>
2018-07-16 18:18:11 -0700

    added details to home page

--- client/src/planning/HowItWorks.js
+++ client/src/planning/HowItWorks.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+// import Signup from './auth/Signup.js';
+import { Link } from 'react-router-dom';
+import Results from './Results';
+import Signup from '../auth/Signup';
 
 class HowItWorks extends Component {
   render() {
@@ -6,9 +10,20 @@ class HowItWorks extends Component {
       <div className="About-container-how">
         <div className="container">
           <div className="row">
-            <div className="how-it-works col s12 m4"><h3>EXPLORE</h3> <i className="how material-icons">language</i></div>
-            <div className="how-it-works col s12 m4"><h3>PLAN</h3> <i className="how material-icons">border_color</i></div>
-            <div className="how-it-works col s12 m4"><h3>ORGANIZE</h3> <i className="how material-icons">clear_all</i></div>
+            <div className="how-it-works col s12 m4">
+              <a href="/results">
+                <p>Search Travel Destinations</p> 
+                  <i className="how material-icons">language</i>
+              </a>
+            </div>
+            <div className="how-it-works col s12 m4">
+              <p>View & Save Top Restaurants</p> 
+                <i className="how material-icons">border_color</i>
+            </div>
+            <div className="how-it-works col s12 m4">
+              <p>Create Your Custom Packing List</p>
+                <i className="how material-icons">clear_all</i>
+            </div>
           </div>
         </div>
       </div>
